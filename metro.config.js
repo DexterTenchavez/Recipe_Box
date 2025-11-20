d5f02f222e4fd4b3aba7a473193a11c28e9ab306
Dexter Tenchavez <dextertenchavez@gmail.com>
2025-11-21 01:22:52 +0800

    adding and edit

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,3 @@
-const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const { getDefaultConfig } = require('@expo/metro-config');
 
-const defaultConfig = getDefaultConfig(__dirname);
-
-const config = {
-  resolver: {
-    assetExts: [...defaultConfig.resolver.assetExts, 'cjs'],
-  },
-};
-
-module.exports = mergeConfig(defaultConfig, config);
+module.exports = getDefaultConfig(__dirname);
